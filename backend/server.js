735e9096b5a5948c21393dafb95e78f2ef12ef14
Varshil-Shah <varshilashah09@gmail.com>
2023-07-31 14:37:33 +0530

    Initialize the socket.io

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const app = require("express")();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  console.log("What is socket:", socket);
+  console.log("Socket is active to connect!");
+
+  socket.on("chat", (payload) => {
+    console.log("What is payload:", payload);
+    io.emit("chat", payload);
+  });
+});
+
+// app.listen(3000, () => console.log("Server running..."));
+server.listen(5000, () => console.log("Server running on port 5000..."));
