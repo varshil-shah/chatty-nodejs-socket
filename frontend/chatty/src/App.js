54d580df89816d67894e299db7ba577f2fc9d401
Varshil-Shah <varshilashah09@gmail.com>
2023-07-31 15:22:18 +0530

    Implement simple chat UI for sending and receiving messages

---
+++ frontend/chatty/src/App.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { nanoid } from "nanoid";
+import "./App.css";
+
+const socket = io.connect("http://localhost:5000");
+const username = nanoid(4);
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  const sendChat = (e) => {
+    e.preventDefault();
+    socket.emit("chat", { message, username });
+    setMessage("");
+  };
+
+  useEffect(() => {
+    socket.on("chat", (payload) => {
+      setChat([...chat, payload]);
+    });
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Chatty App</h1>
+
+        {chat.map((payload, index) => {
+          return (
+            <p key={index}>
+              {payload.message}: <span>id: {payload.username}</span>
+            </p>
+          );
+        })}
+
+        <form onSubmit={sendChat}>
+          <input
+            type="text"
+            name="chat"
+            value={message}
+            placeholder="Enter message"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit">Send</button>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
